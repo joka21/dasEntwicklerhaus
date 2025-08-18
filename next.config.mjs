8689d1129a86af3ae4f70823b869800db55f9095
joka21 <jo.kalenberb@web.de>
2025-08-18 16:25:13 +0200

    neuer Versuch dieIdee

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  trailingSlash: false
+  trailingSlash: false,
+  async redirects() {
+    return [
+      {
+        source: '/dieIdee',
+        destination: '/dieidee',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
