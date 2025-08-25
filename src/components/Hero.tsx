97555e75a967a8b6ee7f3d5d3c4c4f035c7b3f34
joka21 <jo.kalenberb@web.de>
2025-08-25 12:31:02 +0200

    hero3

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,32 +14,32 @@ export default function Hero() {
   return (
     <section className="relative h-screen bg-primary overflow-hidden flex items-center justify-center">
       {/* Geometric Shapes */}
-      {/* Green Rectangle - Top Left */}
+      {/* Green Rectangle - Top Left Corner */}
       <div 
-        className="absolute top-8 left-8 lg:top-16 lg:left-16 w-24 h-16 lg:w-32 lg:h-20 bg-umwelt transition-transform duration-300 hover:scale-110 hover:rotate-3"
+        className="absolute top-0 left-0 w-32 h-24 lg:w-48 lg:h-32 bg-umwelt transition-transform duration-300 hover:scale-110 hover:rotate-3"
         style={{
           transform: `translateY(${scrollY * 0.2}px) rotate(${scrollY * 0.1}deg)`
         }}
       ></div>
 
-      {/* Purple/Violet Circle - Top Right */}
+      {/* Purple/Violet Half-Circle - Right Edge */}
       <div 
-        className="absolute top-12 right-8 lg:top-20 lg:right-16 w-20 h-20 lg:w-28 lg:h-28 bg-frieden rounded-full transition-transform duration-300 hover:scale-110"
+        className="absolute top-16 -right-16 lg:top-20 lg:-right-20 w-32 h-32 lg:w-48 lg:h-48 bg-frieden rounded-full transition-transform duration-300 hover:scale-110"
         style={{
           transform: `translateY(${scrollY * -0.15}px) translateX(${scrollY * -0.1}px)`
         }}
       ></div>
 
-      {/* Yellow Triangle - Bottom */}
+      {/* Yellow Triangle - Bottom Edge */}
       <div 
-        className="absolute bottom-16 left-1/2 transform -translate-x-1/2 lg:bottom-20 transition-transform duration-300 hover:scale-110 hover:-rotate-12"
+        className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 lg:-bottom-12 transition-transform duration-300 hover:scale-110 hover:-rotate-12"
         style={{
           transform: `translateX(-50%) translateY(${scrollY * -0.3}px)`,
           width: 0,
           height: 0,
-          borderLeft: '40px solid transparent',
-          borderRight: '40px solid transparent',
-          borderBottom: '60px solid',
+          borderLeft: '60px solid transparent',
+          borderRight: '60px solid transparent',
+          borderBottom: '80px solid',
           borderBottomColor: 'var(--menschen)',
         }}
       ></div>
@@ -73,7 +73,7 @@ export default function Hero() {
       {/* Additional decorative shapes for visual interest */}
       {/* Small accent circle - left side */}
       <div 
-        className="absolute top-1/3 left-4 lg:left-12 w-6 h-6 lg:w-8 lg:h-8 bg-zusammenarbeit rounded-full opacity-60 transition-all duration-500 hover:opacity-100 hover:scale-150"
+        className="absolute top-1/3 left-2 lg:left-12 w-8 h-8 lg:w-12 lg:h-12 bg-zusammenarbeit rounded-full opacity-60 transition-all duration-500 hover:opacity-100 hover:scale-150"
         style={{
           transform: `translateY(${scrollY * 0.4}px)`
         }}
@@ -81,7 +81,7 @@ export default function Hero() {
 
       {/* Small accent square - right side */}
       <div 
-        className="absolute bottom-1/3 right-6 lg:right-20 w-8 h-8 lg:w-12 lg:h-12 bg-zukunft opacity-40 rotate-45 transition-all duration-700 hover:opacity-80 hover:rotate-180 hover:scale-125"
+        className="absolute bottom-1/3 right-2 lg:right-20 w-12 h-12 lg:w-16 lg:h-16 bg-zukunft opacity-40 rotate-45 transition-all duration-700 hover:opacity-80 hover:rotate-180 hover:scale-125"
         style={{
           transform: `rotate(45deg) translateY(${scrollY * -0.25}px)`
         }}
