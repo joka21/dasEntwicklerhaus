5094ca67ba0eb85b02e10babca5a4f7b054587b7
joka21 <jo.kalenberb@web.de>
2025-08-25 14:01:26 +0200

    main2

--- src/components/ContentSection.tsx
+++ src/components/ContentSection.tsx
@@ -10,14 +10,14 @@ export default function ContentSection({ children, className = '' }: ContentSect
     <section className={`relative w-full bg-light text-dark overflow-hidden ${className}`}>
       {/* Background Geometric Decorations */}
       {/* Green Rectangles */}
-      <div className="absolute top-16 left-8 w-12 h-8 bg-umwelt opacity-20 rotate-12 z-0"></div>
-      <div className="absolute top-32 right-20 w-16 h-10 bg-umwelt opacity-15 -rotate-6 z-0"></div>
-      <div className="absolute bottom-24 left-1/4 w-14 h-9 bg-umwelt opacity-25 rotate-45 z-0"></div>
+      <div className="absolute top-16 left-8 w-16 h-12 bg-umwelt-600 opacity-20 rotate-12 z-0"></div>
+      <div className="absolute top-32 right-20 w-20 h-14 bg-umwelt-700 opacity-15 -rotate-6 z-0"></div>
+      <div className="absolute bottom-24 left-1/4 w-18 h-12 bg-umwelt-500 opacity-25 rotate-45 z-0"></div>
       
       {/* Purple Circles */}
-      <div className="absolute top-20 right-12 w-10 h-10 bg-frieden rounded-full opacity-20 z-0"></div>
-      <div className="absolute top-1/3 left-12 w-8 h-8 bg-frieden rounded-full opacity-15 z-0"></div>
-      <div className="absolute bottom-16 right-1/3 w-12 h-12 bg-frieden rounded-full opacity-25 z-0"></div>
+      <div className="absolute top-20 right-12 w-14 h-14 bg-frieden-600 rounded-full opacity-20 z-0"></div>
+      <div className="absolute top-1/3 left-12 w-12 h-12 bg-frieden-700 rounded-full opacity-15 z-0"></div>
+      <div className="absolute bottom-16 right-1/3 w-16 h-16 bg-frieden-500 rounded-full opacity-25 z-0"></div>
       
       {/* Yellow Triangles */}
       <div 
@@ -25,10 +25,10 @@ export default function ContentSection({ children, className = '' }: ContentSect
         style={{
           width: 0,
           height: 0,
-          borderLeft: '15px solid transparent',
-          borderRight: '15px solid transparent',
-          borderBottom: '20px solid',
-          borderBottomColor: 'var(--menschen)',
+          borderLeft: '20px solid transparent',
+          borderRight: '20px solid transparent',
+          borderBottom: '28px solid',
+          borderBottomColor: 'var(--menschen-600)',
         }}
       ></div>
       <div 
@@ -36,26 +36,26 @@ export default function ContentSection({ children, className = '' }: ContentSect
         style={{
           width: 0,
           height: 0,
-          borderLeft: '20px solid transparent',
-          borderRight: '20px solid transparent',
-          borderBottom: '25px solid',
-          borderBottomColor: 'var(--menschen)',
+          borderLeft: '26px solid transparent',
+          borderRight: '26px solid transparent',
+          borderBottom: '32px solid',
+          borderBottomColor: 'var(--menschen-700)',
         }}
       ></div>
       
       {/* Blue Future Shapes */}
-      <div className="absolute top-24 left-2/3 w-6 h-16 bg-zukunft opacity-20 rotate-12 z-0"></div>
-      <div className="absolute bottom-20 right-16 w-10 h-6 bg-zukunft opacity-15 -rotate-45 z-0"></div>
+      <div className="absolute top-24 left-2/3 w-8 h-20 bg-zukunft-600 opacity-20 rotate-12 z-0"></div>
+      <div className="absolute bottom-20 right-16 w-14 h-8 bg-zukunft-700 opacity-15 -rotate-45 z-0"></div>
       
       {/* Gray Secondary Accents */}
-      <div className="absolute top-1/2 right-8 w-4 h-4 bg-secondary rounded-full opacity-30 z-0"></div>
-      <div className="absolute bottom-1/3 left-8 w-8 h-2 bg-secondary opacity-20 rotate-45 z-0"></div>
-      <div className="absolute top-12 left-1/2 w-6 h-6 bg-secondary opacity-25 rotate-12 z-0"></div>
+      <div className="absolute top-1/2 right-8 w-6 h-6 bg-secondary-600 rounded-full opacity-30 z-0"></div>
+      <div className="absolute bottom-1/3 left-8 w-12 h-4 bg-secondary-700 opacity-20 rotate-45 z-0"></div>
+      <div className="absolute top-12 left-1/2 w-8 h-8 bg-secondary-500 opacity-25 rotate-12 z-0"></div>
       
       {/* Additional Organic Shapes */}
-      <div className="absolute top-2/3 right-1/4 w-5 h-12 bg-zusammenarbeit opacity-15 rotate-75 z-0"></div>
-      <div className="absolute bottom-8 left-2/3 w-3 h-3 bg-umwelt rounded-full opacity-30 z-0"></div>
-      <div className="absolute top-8 right-1/3 w-7 h-3 bg-frieden opacity-10 -rotate-12 z-0"></div>
+      <div className="absolute top-2/3 right-1/4 w-7 h-16 bg-zusammenarbeit-600 opacity-15 rotate-75 z-0"></div>
+      <div className="absolute bottom-8 left-2/3 w-5 h-5 bg-umwelt-800 rounded-full opacity-30 z-0"></div>
+      <div className="absolute top-8 right-1/3 w-10 h-5 bg-frieden-800 opacity-10 -rotate-12 z-0"></div>
       
       {/* Content Container */}
       <div className="relative z-10 mx-auto w-[90%] max-w-[1500px] py-16">
